ddda0cc052e5a1b66fe94f920b09b69ff74073bf
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-02-02 21:00:29 +0100

    Add release script (local use only!)

---
+++ scripts/publish_to_s3.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+latest=$(gh release list | head -1 | awk '{print $1}')
+echo "Downloading ${latest}..."
+
+mkdir "${latest}"
+pushd "${latest}"
+gh release download "${latest}" -p "*.zip"
+popd
+
+ls -l "${latest}"
+export AWS_PROFILE=bricks
+s3cmd sync -v "${latest}" s3://databricks-bricks/
