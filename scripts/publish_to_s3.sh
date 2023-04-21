7b24511fc6a614a1de306877957d4aac19db836c
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-04-21 14:40:49 +0200

    Include index.html in publish

--- scripts/publish_to_s3.sh
+++ scripts/publish_to_s3.sh
@@ -10,6 +10,9 @@ pushd "${latest}"
 gh release download "${latest}" -p "*.zip"
 popd
 
+version=$(sed -e 's/v//' <<<"$latest")
+sed -e "s/\$version/$version/g" ./index.html > $latest/index.html
+
 ls -l "${latest}"
 export AWS_PROFILE=bricks
 s3cmd sync -v "${latest}" s3://databricks-bricks/
